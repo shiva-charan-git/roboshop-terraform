9cde288ec71d2138215e0c9e3716a574ab0ab1ae
shiva-charan-git <shivacharan021@gmail.com>
2023-06-08 03:13:50 +0530

    M

--- ec2/main.tf
+++ ec2/main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "ec2" {
 }
 
 resource "null_resource" "nulll" {
-    provisioner ""remote-exec"" {
+    provisioner "remote-exec" {
     connection {
        host= aws_instance.ec2.public_ip
        user = "centos"
