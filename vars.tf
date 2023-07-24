d44e2ecc0f509c42790c71ca8724c0f9c04dba9c
shiva-charan-git <shivacharan021@gmail.com>
2023-07-24 16:39:50 +0530

    M

--- vars.tf
+++ vars.tf
@@ -12,37 +12,37 @@ variable "instances" {
       name = "catalogue"
       type = "t3.small"
     }
-    #  redis ={
-    #   name = "redis"
-    #   type = "t3.small"
-    # }
-    #  user ={
-    #   name = "user"
-    #   type = "t3.small"
-    # }
-    #  cart ={
-    #   name = "cart"
-    #   type = "t3.small"
-    # }
-    #  mysql ={
-    #   name = "mysql"
-    #   type = "t3.small"
-    #   password = "RoboShop@1"
-    # }
-    #  shipping ={
-    #   name = "shipping"
-    #   type = "t3.small"
-    #   password = "RoboShop@1"
-    # }
-    #  rabbitmq ={
-    #   name = "rabbitmq"
-    #   type = "t3.small"
-    #   password = "roboshop123"
-    # }
-    #  payment ={
-    #   name = "payment"
-    #   type = "t3.small"
-    #   password = "roboshop123"
-    # }
+     redis ={
+      name = "redis"
+      type = "t3.small"
+    }
+     user ={
+      name = "user"
+      type = "t3.small"
+    }
+     cart ={
+      name = "cart"
+      type = "t3.small"
+    }
+     mysql ={
+      name = "mysql"
+      type = "t3.small"
+      password = "RoboShop@1"
+    }
+     shipping ={
+      name = "shipping"
+      type = "t3.small"
+      password = "RoboShop@1"
+    }
+     rabbitmq ={
+      name = "rabbitmq"
+      type = "t3.small"
+      password = "roboshop123"
+    }
+     payment ={
+      name = "payment"
+      type = "t3.small"
+      password = "roboshop123"
+    }
   }
 }
